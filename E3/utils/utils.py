8db4b27aa1463fb502932dde861f499cc8921b5c
liucong <liucong.luke@gmail.com>
2024-10-02 13:15:19 +0200

    add read_csv function

--- E3/utils/utils.py
+++ E3/utils/utils.py
@@ -1,2 +1,14 @@
-def print_help():
-    print('help')
+import os
+import sys 
+import numpy as np
+
+def read_csv(fileName):
+  csv_path = os.path.join(sys.path[0], 'auxFiles', fileName)
+  fr = open(csv_path)
+  lines = fr.readlines()
+  dataArr = []
+  for line in lines:
+    listFromLine = line.strip().split('\t')
+    dataArr.append(listFromLine)
+
+  return dataArr[2:]
