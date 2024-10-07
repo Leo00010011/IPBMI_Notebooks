f38a134c6ad7d6e568576cfe539604306a51e2c5
lulloa <lulloa@datamart.co>
2024-10-07 23:08:53 +0200

    Phantom already working

--- E3/phantomsFnc.py
+++ E3/phantomsFnc.py
@@ -1,48 +1,95 @@
 import numpy as np
-from utils import utils
+from utils.utils import get_coef
+
 
 def cube_phantom_h(edge_size, energy):
-  soft_coef = utils.get_coef(index=4, energy=energy)
-  bone_coef = utils.get_coef(index=2, energy=energy)
-  if edge_size > 2 and np.log2(edge_size) % 1 == 0:
-    soft_tissue_size = edge_size
-    bone_size = int(edge_size / 2)
-    tissue = np.zeros((soft_tissue_size, soft_tissue_size, soft_tissue_size))
-    tissue[:] = soft_coef
-    offset = int((soft_tissue_size - bone_size) / 2)
-    tissue[offset:offset + bone_size, offset: offset+bone_size, offset:offset+bone_size] = bone_coef
-    # normalize
-    # tissue = utils.normalize_data(tissue)
-    return tissue
-  else:
-    print('not powers of 2')
-    return
-  
+    soft_coef = get_coef(index=4, energy=energy)
+    bone_coef = get_coef(index=2, energy=energy)
+    if edge_size > 2 and np.log2(edge_size) % 1 == 0:
+        soft_tissue_size = edge_size
+        bone_size = int(edge_size / 2)
+        tissue = np.zeros(
+            (soft_tissue_size, soft_tissue_size, soft_tissue_size))
+        tissue[:] = soft_coef
+        offset = int((soft_tissue_size - bone_size) / 2)
+        tissue[offset:offset + bone_size, offset: offset +
+               bone_size, offset:offset+bone_size] = bone_coef
+        # normalize
+        # tissue = utils.normalize_data(tissue)
+        return tissue
+    else:
+        print('not powers of 2')
+        return
+
+
 def cube_phantom_nh(edge_size, energy):
-  soft_coef = utils.get_coef(4, energy)
-  air_coef = utils.get_coef(1, energy)
-  water_coef = utils.get_coef(5, energy)
-  if edge_size > 2 and np.log2(edge_size) % 1 == 0:
-    tissue_edge = int(edge_size / 2)
-    offset = int((edge_size - tissue_edge) / 2)
-    phantom = np.zeros((edge_size, edge_size, edge_size))
-    phantom[:, :, :] = water_coef
-    phantom[:, :, tissue_edge:] = air_coef
-    phantom[offset:offset + tissue_edge, offset: offset+tissue_edge, offset:offset+tissue_edge] = soft_coef
-    # phantom = utils.normalize_data(phantom)
-    return phantom
-  else:
-    print('not powers of 2')
-    return 
-  
+    soft_coef = get_coef(4, energy)
+    air_coef = get_coef(1, energy)
+    water_coef = get_coef(5, energy)
+    if edge_size > 2 and np.log2(edge_size) % 1 == 0:
+        tissue_edge = int(edge_size / 2)
+        offset = int((edge_size - tissue_edge) / 2)
+        phantom = np.zeros((edge_size, edge_size, edge_size))
+        phantom[:, :, :] = water_coef
+        phantom[:, :, tissue_edge:] = air_coef
+        phantom[offset:offset + tissue_edge, offset: offset +
+                tissue_edge, offset:offset+tissue_edge] = soft_coef
+        # phantom = utils.normalize_data(phantom)
+        return phantom
+    else:
+        print('not powers of 2')
+        return
+
 
 def breast_phantom(edge_size, energy):
     cell_size = edge_size//18
-    frame = np.full((edge_size, edge_size, edge_size),1)
+    frame = np.full((edge_size, edge_size, edge_size), 1)
     mid_point = edge_size//2
     # putting breast base
-    frame[mid_point - 6*cell_size: mid_point + 3*cell_size,
-          mid_point - 4*cell_size: mid_point + 3*cell_size,
+    frame[mid_point - 4*cell_size: mid_point + 3*cell_size,
+          mid_point - 6*cell_size: mid_point + 3*cell_size,
           mid_point - 3*cell_size: mid_point + 4*cell_size] = 0
     return frame
+
+def breast_phantom(edge_size, energy):
+    breast_coef = get_coef(3,energy)
+    air_coef = get_coef(1,energy)
+    soft_coef = get_coef(4,energy)
+    adipose_coef = get_coef(0,energy)
+
+
+    cell_size = edge_size//18
+    frame = np.full((edge_size, edge_size, edge_size), air_coef)
+    mid_point = edge_size//2
+
+    # putting breast adipose 
+    frame[mid_point - 4*cell_size: mid_point + 3*cell_size,
+          mid_point - 3*cell_size: mid_point + 4*cell_size,
+          mid_point - 6*cell_size: mid_point + 3*cell_size] = adipose_coef
+      
+    # putting breast tissue 
+    frame[mid_point - 2*cell_size: mid_point + cell_size,
+          mid_point - cell_size: mid_point + 2*cell_size,
+          mid_point - 5*cell_size: mid_point - 2*cell_size] = breast_coef
     
+    # putting soft tissue
+    frame[mid_point - cell_size: mid_point,
+          mid_point: mid_point + cell_size,
+          mid_point - 4*cell_size: mid_point - 3*cell_size] = soft_coef
+
+    # putting right most square
+    frame[mid_point - 2*cell_size: mid_point + cell_size,
+          mid_point - cell_size: mid_point + 2*cell_size,
+          mid_point + 3*cell_size: mid_point + 6*cell_size] = adipose_coef
+      
+    # putting second soft tissue
+    frame[mid_point - cell_size: mid_point,
+          mid_point: mid_point + cell_size,
+          mid_point - cell_size: mid_point] = soft_coef
+
+    # putting third soft tissue
+    frame[mid_point - cell_size: mid_point,
+          mid_point: mid_point + cell_size,
+          mid_point + 4*cell_size: mid_point + 5*cell_size] = soft_coef
+    
+    return frame
--- E3/utils/utils.py
+++ E3/utils/utils.py
@@ -1,5 +1,5 @@
 import os
-import sys 
+import sys
 import numpy as np
 
 TISSUE_LABEL = {0: 'Adipose',
@@ -7,32 +7,41 @@ TISSUE_LABEL = {0: 'Adipose',
                 2: 'Bone',
                 3: 'Breast',
                 4: 'Soft',
-                5: 'Water' }
+                5: 'Water'}
 
-def get_csv_data(tissue_index):
-  csv_path = os.path.join(sys.path[0], 'auxFiles', 'coefAtenuacion' + TISSUE_LABEL[tissue_index] + '.csv')
-  dataArr = []
-  with open(csv_path, 'r') as file:
-    next(file)
-    next(file)
-    for line in file:
-      line = line.strip().split('\t')
-      listFromLine = [float(item) for item in line]
-      dataArr.append(listFromLine)
-  return dataArr
 
-def get_coef(index, energy):
-  data = get_csv_data(index)
-  coef = 0
-  for item in data:
-    if item[0] == energy:
-      coef = item[1]
-      break
-  return coef
+def get_csv_data(tissue_index, csv_path=None):
+    if not csv_path:
+        csv_path = os.path.join(
+            sys.path[0], 'auxFiles', 'coefAtenuacion' + TISSUE_LABEL[tissue_index] + '.csv')
+    else:
+        csv_path = os.path.join(
+            csv_path, 'coefAtenuacion' + TISSUE_LABEL[tissue_index] + '.csv')
+    dataArr = []
+    with open(csv_path, 'r') as file:
+        next(file)
+        next(file)
+        for line in file:
+            line = line.strip().split('\t')
+            listFromLine = [float(item) for item in line]
+            dataArr.append(listFromLine)
+    return dataArr
+
+
+def get_coef(index, energy, csv_path=None):
+    data = get_csv_data(index, csv_path)
+    coef = 0
+    for item in data:
+        if item[0] == energy:
+            coef = item[1]
+            break
+    return coef
+
 
 def normalize_data(data):
-  return np.round(data/data.max(), 2) * 256
+    return np.round(data/data.max(), 2) * 256
+
 
 def getLine(data, nLine):
-  lines = data[:, nLine]
-  return lines
+    lines = data[:, nLine]
+    return lines
