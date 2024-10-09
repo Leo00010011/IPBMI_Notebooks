a41bfee71b01a6ac2005e92f638c2ad343ff612b
liucong <liucong.luke@gmail.com>
2024-10-09 16:09:22 +0200

    add createQuantumImage function

--- E4/auxFiles/phantomsFnc.py
+++ E4/auxFiles/phantomsFnc.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def createQuantumImage(N0, n, pointSize):
-  N1 = N0 * pointSize
-  quantum_image = np.random.uniform(N1, n)
+  N1 = N0 * (pointSize ** 2)
+  quantum_image = np.random.poisson(N1, n)
   return quantum_image
