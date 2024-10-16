a71ffc067b59c86e55e6b15dbf877801825e9351
liucong <liucong.luke@gmail.com>
2024-10-16 12:32:28 +0200

    add auxiliary functions

---
+++ E5/auxFiles/auxFnc.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os, sys
+
+TISSUE_LABEL = {0: 'Adipose',
+                1: 'Air',
+                2: 'Bone',
+                3: 'Breast',
+                4: 'Soft',
+                5: 'Water'}
+
+def get_csv_data(tissue_index, csv_path=None):
+    if not csv_path:
+        csv_path = os.path.join(
+            sys.path[0], 'auxFiles', 'coef', 'coefAtenuacion' + TISSUE_LABEL[tissue_index] + '.csv')
+    else:
+        csv_path = os.path.join(
+            csv_path, 'coef', 'coefAtenuacion' + TISSUE_LABEL[tissue_index] + '.csv')
+    dataArr = []
+    with open(csv_path, 'r') as file:
+        next(file)
+        next(file)
+        for line in file:
+            line = line.strip().split('\t')
+            listFromLine = [float(item) for item in line]
+            dataArr.append(listFromLine)
+    return dataArr
+
+def get_coef(index, energy, csv_path=None):
+    data = get_csv_data(index, csv_path)
+    coef = 0
+    for item in data:
+        if item[0] == energy:
+            print(item[0])
+            print(item[1])
+            coef = item[1]
+            break
+    if coef == 0:
+        coef = np.interp(energy,[item[0] for item in data], [item[1] for item in data])
+    return coef
--- E5/auxFiles/phantomsFnc.py
+++ E5/auxFiles/phantomsFnc.py
@@ -1,6 +1,30 @@
 import numpy as np
+from auxFiles.auxFnc import get_coef
 
 def source(Kpv, I0):
   N0 = I0
   eE = Kpv * 0.4
   return N0, eE
+
+def cube_phantom_nh(edge_size, energy):
+    soft_coef = get_coef(4, energy)
+    air_coef = get_coef(1, energy)
+    water_coef = get_coef(5, energy)
+    print(soft_coef, air_coef, water_coef)
+    if edge_size > 2 and np.log2(edge_size) % 1 == 0:
+        tissue_edge = int(edge_size / 2)
+        offset = int((edge_size - tissue_edge) / 2)
+        phantom = np.zeros((edge_size, edge_size, edge_size))
+        phantom[:, :, :] = water_coef
+        phantom[:, :, tissue_edge:] = air_coef
+        phantom[offset:offset + tissue_edge, offset: offset +
+                tissue_edge, offset:offset+tissue_edge] = soft_coef
+        return phantom
+    else:
+        print('not powers of 2')
+        return
+    
+def interactor_PR(N0, obj, prj):
+    print('N0:',N0)
+    print('obj:',obj)
+    print('prj:',prj)
