b6f0b17ab7fc3ed11830db8e550cde4764b6de3a
lulloa <lulloa@datamart.co>
2024-11-13 12:22:01 +0100

    first version the other functions

--- E8/auxFiles/CTFnc.py
+++ E8/auxFiles/CTFnc.py
@@ -3,3 +3,13 @@ try:
     from auxFiles.auxFnc import *
 except:
     from auxFnc import *
+
+def displayWL(Image, W, L, maxGL):
+    min_intensity = L - W/2
+    Image -= min_intensity
+    Image = np.clip(Image, 0, W)
+    return Image/W*maxGL
+
+def setHounsfield(Image, eE):
+    water_coef = get_coef(5,eE)
+    return (Image - water_coef)/water_coef*1000
